93118c1bbb9147e6b08c8fb47f5d4ffc27eb7b02
Eng-Farooq <107753135+Eng-Farooq@users.noreply.github.com>
2022-09-12 13:38:06 +0300

    llla

---
+++ wp-content/themes/marketing-techup/functions.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Child Theme functions and definitions.
+ * This theme is a child theme for Deneb.
+ *
+ * @subpackage Entertainment techup
+ * @author  wptexture http://testerwp.com/
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GNU Public License
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * When using a child theme you can override certain functions (those wrapped
+ * in a function_exists() call) by defining them first in your child theme's
+ * functions.php file. The child theme's functions.php file is included before
+ * the parent theme's file, so the child theme functions would be used.
+ */
+
+/**
+ * Theme functions and definitions.
+ */
+ 
+add_action( 'wp_enqueue_scripts', 'marketing_techup_child_css',25);
+function marketing_techup_child_css() {
+	wp_enqueue_style( 'marketing-techup-parent-theme-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'marketing-techup-child-style',get_stylesheet_directory_uri() . '/child-css/child.css');
+	wp_enqueue_script( 'marketing-techup-custom-script', get_stylesheet_directory_uri() . '/child-js/custom-script.js');
+ 
+}
+
+/**
+ * Custom Hooks defined 
+ */
+require( get_stylesheet_directory() . '/custom-footer/footer-hooks.php');
